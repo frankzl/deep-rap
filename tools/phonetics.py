25c3d7d00895203a49fe3d7c20fd48aff00a581d
René Lalla <r.lalla@gmx.de>
2019-03-05 15:17:59 +0100

    update phonetics

--- tools/phonetics.py
+++ tools/phonetics.py
@@ -2,24 +2,62 @@ import nltk
 from nltk.corpus import cmudict as cmu
 
 arpabet = cmu.dict()
+unknown_words = set()
 
 def get_phonem(text):
     try:
         return arpabet[text][0]
     except:
+        unknown_words.add(text)
         return ""
+
 
 def get_phonem_string(text):
     try:
         return "".join(arpabet[text][0])
     except:
+        unknown_words.add(text)
         return ""
+
 
 def get_phonem_string_spaced(text):
     try:
         return " ".join(arpabet[text][0])
     except:
+        unknown_words.add(text)
         return ""
+
+
+def get_unknown_words():
+    return list(unknown_words)
+
+
+def get_unkowns_as_list():
+    if len(unknown_words) > 0:
+        return "\n".join(get_unknown_words())
+    return []
+
+
+# use logios lextool to get generated phonetics
+# http://www.speech.cs.cmu.edu/tools/lextool.html
+def create_unknown_dict_from_text(unknown_list):
+    unknown_dict = {}
+    splits = unknown_list.split("\n")
+    for split in splits:
+        try:
+            word, phonem = split.split(":")
+        except:
+            print(split)
+        unknown_dict[word.lower()] = [phonem.split(" ")]
+    return unknown_dict
+
+
+def update_arpabet(dict):
+    global arpabet
+    global unknown_words
+    arpabet = {**arpabet, **dict}
+    unknown_words = set()
+    
 
 # get ryhme for word
 # level represents accuracy of rhyme: take the last {level} phonems to compare
