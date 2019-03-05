65fb54dd56f0635d9b943226bbcd04a4633b6c1c
René Lalla <r.lalla@gmx.de>
2019-03-05 11:10:46 +0100

    added phonetics lib

---
+++ tools/phonetics.py
@@ -0,0 +1,32 @@
+import nltk
+from nltk.corpus import cmudict as cmu
+
+arpabet = cmu.dict()
+
+def get_phonem(text):
+    try:
+        return arpabet[text][0]
+    except:
+        return text
+
+def get_phonem_string(text):
+    try:
+        return "".join(arpabet[text][0])
+    except:
+        return text
+
+def get_phonem_string_spaced(text):
+    try:
+        return " ".join(arpabet[text][0])
+    except:
+        return text
+
+# get ryhme for word
+# level represents accuracy of rhyme: take the last {level} phonems to compare
+def rhyme(inp, level):
+    entries = cmu.entries() # [(word, phonetics),...]
+    syllables = [(word, syl) for word, syl in entries if word == inp]
+    rhymes = []
+    for (word, syllable) in syllables:
+        rhymes += [word for word, pron in entries if pron[-level:] == syllable[-level:]]
+    return list(set(rhymes))
